df47477499953fa8df6da9da54f6cc5260efe53c
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-04-18 00:39:20 +0900

    [DEV 1] project init

---
+++ src/main/java/com/oneroom/webapp/RoomBrokerageWebappApplication.java
@@ -0,0 +1,13 @@
+package com.oneroom.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBrokerageWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBrokerageWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneroom/webapp/RoomBrokerageWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneroom.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBrokerageWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
